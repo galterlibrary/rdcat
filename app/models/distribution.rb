65f6170d32996127e37911901f3f334835b84ca7
Piotr Hebal <phebal@gmail.com>
2017-11-16 14:47:36 -0600

    Determine mime type and format when saving Distributions Part of #26
    Use `file' to determine MIME type and format when not specified.
    Including tests with carrierwave matchers.

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -19,13 +19,26 @@ class Distribution < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :dataset, presence: true
 
+  before_save :check_content_type_and_format
+
+  def check_content_type_and_format
+    return unless self.artifact.current_path
+    if self.artifact.content_type.blank?
+      self.artifact.file.content_type = `file -b --mime-type #{artifact.current_path}`.to_s.strip
+    end
+    if self.format.blank?
+      self.format = `file -b #{artifact.current_path}`.to_s.strip
+    end
+  end
+  private :check_content_type_and_format
+
   after_save do
     if self.dataset.present?
       self.dataset.__elasticsearch__.index_document
     end
   end
-  around_destroy :update_dataset_index
 
+  around_destroy :update_dataset_index
   def update_dataset_index
     ds = self.dataset
     yield
--- spec/models/distribution_spec.rb
+++ spec/models/distribution_spec.rb
@@ -12,13 +12,66 @@
 #  updated_at  :datetime         not null
 #  artifact    :string
 #
-
 require 'rails_helper'
+require 'carrierwave/test/matchers'
 
 RSpec.describe Distribution, :type => :model do
+  include CarrierWave::Test::Matchers
+
+  before do
+    stub_request(:put, /localhost:9250/)
+  end
+
   it { should belong_to(:dataset) }
 
   it { should validate_uniqueness_of(:name) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:dataset) }
+
+  describe 'when no artifact' do
+    context 'when no format passed' do
+      subject do
+        FactoryGirl.create(:distribution, format: nil)
+      end
+
+      it 'leaves the format blank' do
+        expect(subject.format).to be_blank
+      end
+    end
+
+    context 'when format passed' do
+      subject do
+        FactoryGirl.create(:distribution, format: 'Format!')
+      end
+
+      it 'uses the passed format' do
+        expect(subject.format).to eq('Format!')
+      end
+    end
+  end
+
+  describe 'with artifact' do
+    subject { FactoryGirl.create(:distribution, format: nil) }
+
+    before do
+      File.open('spec/fixtures/artifact1.txt') { |f|
+        subject.artifact.store!(f)
+      }
+      subject.save!
+    end
+
+    it 'fills out the format and content type of the artifact file' do
+      expect(subject.artifact.content_type).to eq('text/plain')
+      expect(subject.format).to eq('ASCII text')
+    end
+
+    context 'when format is specified' do
+      subject { FactoryGirl.create(:distribution, format: 'Dry') }
+
+      it 'fills out the content type of the artifact file only' do
+        expect(subject.artifact.content_type).to eq('text/plain')
+        expect(subject.format).to eq('Dry')
+      end
+    end
+  end
 end
