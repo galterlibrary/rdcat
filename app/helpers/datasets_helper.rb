0c9820b367ee68b5b0e63e5dcd57fbea6eb21368
Piotr Hebal <phebal@gmail.com>
2017-12-13 11:13:30 -0600

    Grouped options for institutes Related to #47

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -7,4 +7,15 @@ module DatasetsHelper
     end
     html
   end
+
+  def institutes_and_centers_select_options
+    grouped_options_for_select(
+      Organization.institute_or_center.group_by(&:group_name).inject({}) {|h, grp|
+        h[grp.first] = grp.second.map {|x| [x.name, x.id] }
+        h
+      },
+      @dataset.orgs(:institute_or_center).pluck(:id)
+    )
+  end
+
 end
